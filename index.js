9b56024c3896802956f7917d6dea60ac726b98a2
Nicholas Muriithi <nickstech707@gmail.com>
2024-03-27 23:41:42 +0300

    Practiced using find() to isolate a specific result

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+// Defines a function to search for a Super Bowl win
+superbowlWin = (record) => {
+  // Use the find method to search the record array for an object where the result is "W" (win)
+  const result = record.find(record => record.result === "W");
+
+  // Checks if a winning record was found and return the year of the win. If no win is found, return undefined
+  return !!result ? result.year : undefined;
+}
